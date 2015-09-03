26561bac4ee7d0a35a621e6bfa8494fef9f32efb
shadowgun <shadowgun@i.ua>
2015-09-03 15:04:54 +0300

    update

---
+++ Example/Getting Started/app.js
@@ -0,0 +1,42 @@
+///<reference path="../../Typejector/Compiled/typejector.d.ts"/>
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
+    switch (arguments.length) {
+        case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
+        case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
+        case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
+    }
+};
+var injection = Typejector.Annotation.injection;
+var inject = Typejector.Annotation.inject;
+var context, noiseMaker;
+var SimpleNoiser = (function () {
+    function SimpleNoiser() {
+        this.stringField = "SimpleClass";
+    }
+    SimpleNoiser.prototype.makeNoise = function () {
+        alert("Noise from " + this.stringField);
+    };
+    SimpleNoiser = __decorate([
+        injection
+    ], SimpleNoiser);
+    return SimpleNoiser;
+})();
+var NoiseMakerClass = (function () {
+    function NoiseMakerClass() {
+        this.ownNoiser = undefined;
+    }
+    NoiseMakerClass.prototype.doWork = function () {
+        this.ownNoiser.makeNoise();
+    };
+    __decorate([
+        inject(SimpleNoiser)
+    ], NoiseMakerClass.prototype, "ownNoiser");
+    NoiseMakerClass = __decorate([
+        injection
+    ], NoiseMakerClass);
+    return NoiseMakerClass;
+})();
+context = Typejector.getContext();
+noiseMaker = context.getBean(NoiseMakerClass);
+noiseMaker.doWork();
